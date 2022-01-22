b4c9c9c743c610138a0411c51229c8b375eb08b3
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2022-01-22 13:57:37 -0600

    Add Eslint linter locally and linter project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
 
-}
+};
 
 export default App;
