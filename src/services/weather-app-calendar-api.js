a5d676bc2ad0ed61d9b7350b1cc28bda97a391d6
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2022-01-25 03:15:07 -0600

    Create axios request logic for new Reminder (Post /Reminders) API request and create initial logic for new Reminder form

--- src/services/weather-app-calendar-api.js
+++ src/services/weather-app-calendar-api.js
@@ -7,8 +7,21 @@ const API_URL = 'http://127.0.0.1:3001/';
 
 const getReminders = () => axios.get(`${API_URL}reminders`, { headers: authHeader() });
 
+const createReminder = ({
+  description, datetime, city, location_coordinates,
+}) => axios.post(`${API_URL}reminders`, {
+  reminder: {
+    description,
+    datetime,
+    city,
+    location_coordinates,
+  },
+},
+{ headers: authHeader() });
+
 const weatherAppCalendarApi = {
   getReminders,
+  createReminder,
 };
 
 export default weatherAppCalendarApi;
