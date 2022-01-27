f2f85e228f5f53bc7a81180cd880ac404b51e5d9
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2022-01-26 22:34:22 -0600

    Create AXIOS service to handle openWeatherApi requests

---
+++ src/services/openWeatherApi.service.js
@@ -0,0 +1,19 @@
+/* eslint-disable camelcase */
+import axios from 'axios';
+
+const API_URL = 'https://api.openweathermap.org/data/2.5/onecall/';
+
+const getCurrentWeather = ({ lat, lon }) => axios.get(
+  `${API_URL}?lat=${lat}&lon=${lon}&exclude=minutely,hourly,daily&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}`,
+);
+
+const getDailyWeather = ({ lat, lon }) => axios.get(
+  `${API_URL}?lat=${lat}&lon=${lon}&exclude=minutely,hourly,current&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}`,
+);
+
+const openWeatherApiService = {
+  getCurrentWeather,
+  getDailyWeather,
+};
+
+export default openWeatherApiService;
