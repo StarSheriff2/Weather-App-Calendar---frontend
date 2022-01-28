cc53e8187a59cf15373ca82b267ad92de2997d22
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2022-01-28 07:22:48 -0600

    Show weather icon in reminders page

--- src/components/WeatherForecast.js
+++ src/components/WeatherForecast.js
@@ -2,17 +2,15 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import openWeatherApiService from '../services/openWeatherApi.service';
 import { setMessage } from '../slices/message';
-import { remindersState } from '../slices/reminders';
 
 const WeatherForecast = ({
-  coordinates, date, dateTime, weatherData, setWeatherData, city, id,
+  coordinates, date, dateTime, weatherData, setWeatherData, id,
 }) => {
   const getDateDiff = (first, second) => Math.round((second - first) / (1000 * 60 * 60 * 24));
 
   const dispatch = useDispatch();
   const formDate = new Date(date).setHours(0, 0, 0, 0);
   const today = new Date().setHours(0, 0, 0, 0);
-  // const dateObj = new Date(date);
   const dateDiff = getDateDiff(new Date(), dateTime);
   const idIcon = `${id}Icon`;
   const idTemp = `${id}Temp`;
@@ -21,10 +19,9 @@ const WeatherForecast = ({
 
   const [lat, lon] = coordinates.split(', ');
 
-  const exclamationIcon = (<i className="fal fa-exclamation-circle" />);
+  const exclamationIcon = (<i className="fas fa-exclamation-circle" />);
 
   if (formDate < today || dateDiff > 7) {
-    console.log('Im before today and not in next 7 days');
     return (
       <div>
         {exclamationIcon}
@@ -34,17 +31,13 @@ const WeatherForecast = ({
 
   const fetchWeatherData = async () => {
     if (formDate === today) {
-      console.log('Im happening today', city);
       const { data } = await openWeatherApiService.getCurrentWeather({ lat, lon });
       const { weather, main } = data;
       return { icon: weather[0].icon, temp: main.temp };
     }
     const { data } = await openWeatherApiService.getDailyWeather({ lat, lon });
     const { daily: tempByDate } = data;
-    console.log('dateDiff: ', dateDiff);
-    console.log('daily temps: ', tempByDate[dateDiff]);
     const reminderHour = dateTime.getHours();
-    console.log('hour of reminder" ', reminderHour);
     let reminderDatetimeTemp;
 
     if (reminderHour >= 5 && reminderHour < 12) {
@@ -57,25 +50,17 @@ const WeatherForecast = ({
       reminderDatetimeTemp = tempByDate[dateDiff].temp.night;
     }
 
-    console.log({ city, reminderDatetimeTemp });
-
     const { weather } = tempByDate[dateDiff];
     return { icon: weather[0].icon, temp: reminderDatetimeTemp };
-    // return reminderDatetimeTemp;
   };
 
   useEffect(async () => {
     if (Object.keys(weatherData).length === 0 || !(idIcon in weatherData)) {
       try {
         const data = await fetchWeatherData();
-        setLoaded(true);
         const { icon, temp } = data;
-        // const newData = {};
-        // newData[id] = { icon, temp };
-        // console.log('newData Test: ', newData);
         setWeatherData((prevData) => ({ ...prevData, [idIcon]: icon, [idTemp]: temp }));
-        // console.log('data: ', {icon, temp})
-        console.log('weatherData state: ', weatherData);
+        setLoaded(true);
       } catch (error) {
         const message = (error.response
           && error.response.data
@@ -94,8 +79,6 @@ const WeatherForecast = ({
       setLoaded(false);
     };
   }, [weatherData]);
-
-  console.log('response data - temp of day: ', weatherData);
 
   if (!loaded) {
     return (<span className="spinner-border spinner-border-sm" />);
--- src/services/openWeatherApi.service.js
+++ src/services/openWeatherApi.service.js
@@ -8,9 +8,12 @@ const getCurrentWeather = ({ lat, lon }) => axios.get(
   `${CURRENT_WEATHER_API_URL}?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}`,
 );
 
-const getDailyWeather = ({ lat, lon }) => axios.get(
-  `${ONE_CALL_API_URL}?lat=${lat}&lon=${lon}&exclude=minutely,hourly,current&units=metric&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}`,
-);
+const getDailyWeather = ({ lat, lon }) => {
+  console.log('api called');
+  return axios.get(
+    `${ONE_CALL_API_URL}?lat=${lat}&lon=${lon}&exclude=minutely,hourly,current&units=metric&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}`,
+  );
+};
 
 const openWeatherApiService = {
   getCurrentWeather,
