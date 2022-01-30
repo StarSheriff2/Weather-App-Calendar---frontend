8a5f87830a816d8bf670069aaadb3d67d0d8edac
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2022-01-30 01:54:33 -0600

    Change WeatherAPPApi to Production

--- src/services/weather-app-calendar-api.js
+++ src/services/weather-app-calendar-api.js
@@ -2,8 +2,9 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'http://127.0.0.1:3001/';
+// const API_URL = 'http://127.0.0.1:3001/';
 // const API_URL = 'http://0.0.0.0:3001/';
+const API_URL = 'https://weather-app-calendar-api.herokuapp.com/';
 
 const getReminders = () => axios.get(`${API_URL}reminders`, { headers: authHeader() });
 
