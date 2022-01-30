578103e43bca8f02dcc7bed518f56083e5001d66
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2022-01-30 11:55:55 -0600

    Setup test enviroment for Uni tests and snap tests and make first snap test successfully

---
+++ src/__tests__/snapTests/app.snapshot.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { cleanup } from '@testing-library/react';
+import { renderWithRedux } from '../../tests/test-utils';
+import App from '../../App';
+
+afterEach(cleanup);
+
+describe('App', () => {
+  test('Renders the App', () => {
+    const app = renderWithRedux(<App />).toJSON();
+
+    expect(app).toMatchSnapshot();
+  });
+});
---
+++ src/tests/test-utils.js
@@ -0,0 +1,61 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { render as rtlRender } from '@testing-library/react';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import TestRenderer from 'react-test-renderer';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import authReducer from '../slices/auth';
+import messageReducer from '../slices/message';
+import remindersReducer from '../slices/reminders';
+
+const appReducer = combineReducers({
+  auth: authReducer,
+  message: messageReducer,
+  reminders: remindersReducer,
+});
+
+const rootReducer = (state, action) => {
+  if (action.type === 'auth/logout/fulfilled') {
+    return appReducer(undefined, action);
+  }
+  return appReducer(state, action);
+};
+
+const render = (
+  ui,
+  {
+    initialState,
+    store = createStore(
+      rootReducer,
+      initialState,
+      applyMiddleware(thunk),
+    ),
+    ...renderOptions
+  } = {},
+) => {
+  const Wrapper = ({ children }) => (
+    <Provider store={store}>{children}</Provider>
+  );
+
+  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
+};
+
+const renderWithRedux = (
+  component,
+  {
+    initialState,
+    store = configureStore({
+      reducer: rootReducer,
+      middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+      initialState,
+    }),
+  } = {},
+) => ({
+  ...TestRenderer.create(<Provider store={store}>{component}</Provider>),
+});
+
+export * from '@testing-library/react';
+
+export { render, renderWithRedux };
