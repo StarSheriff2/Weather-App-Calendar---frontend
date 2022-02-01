dde3bdc69b8795ec259704a2506fdade484ec237
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2022-02-01 15:31:34 -0600

    Create env files for weathe calendar app api url

--- src/services/weather-app-calendar-api.js
+++ src/services/weather-app-calendar-api.js
@@ -2,9 +2,9 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'http://127.0.0.1:3001/';
+// const API_URL = 'http://127.0.0.1:3001/';
 // const API_URL = 'http://0.0.0.0:3001/';
-// const API_URL = 'https://weather-app-calendar-api.herokuapp.com/';
+const API_URL = process.env.REACT_APP_WEATHER_APP_CALENDAR_API;
 
 const getReminders = () => axios.get(`${API_URL}reminders`, { headers: authHeader() });
 
