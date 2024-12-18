20ccf3162913acaaf60f739e254e5e71fe174279
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2024-12-18 14:51:43 -0600

    feat: update weather app connection

--- src/services/openWeatherApi.service.js
+++ src/services/openWeatherApi.service.js
@@ -1,7 +1,7 @@
 /* eslint-disable camelcase */
 import axios from 'axios';
 
-const ONE_CALL_API_URL = 'https://api.openweathermap.org/data/2.5/onecall';
+const ONE_CALL_API_URL = 'https://api.openweathermap.org/data/3.0/onecall';
 const CURRENT_WEATHER_API_URL =
   'https://api.openweathermap.org/data/2.5/weather';
 
